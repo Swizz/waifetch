4c8f29757f6abc18355d563d41311ee349f4d384
Swizz <Swizz@users.noreply.github.com>
2026-02-17 19:06:08 +0100

    fmt deno code

--- frontend/_config.ts
+++ frontend/_config.ts
@@ -4,7 +4,10 @@ site.add("main.ts")
 
 site.add("main.css")
 
-site.copy("gh:loganmarchione/homelab-svg-assets@0.4.17/assets/*.svg", "loganmarchione/homelab-svg-assets/")
+site.copy(
+	"gh:loganmarchione/homelab-svg-assets@0.4.17/assets/*.svg",
+	"loganmarchione/homelab-svg-assets/",
+)
 
 site.copy("wailsjs")
 
